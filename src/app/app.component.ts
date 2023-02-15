b83cb35b72529466bca68178aca1f3b6eed63c2b
Patrick-Njiru <patricknjiru93@gmail.com>
2023-02-15 07:27:43 +0300

    Create app to practie structural directives.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'structural-directives';
+  // title = 'structural-directives';
+  status = false
+  accepted = "Congratulations, your request has been accepted!"
+  denied = "Sorry, your request has been denied!"
+  notification = ""
+  numbers = [
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten'
+  ]
+  fruit = "apple"
+  luckyNumber = 0
+  acceptance = (value : any) => {
+    this.status = value
+    this.notification = ""
+  }
+  notifyMe = () => this.notification = "🤡 Hello there! Please Click Us"
+  backToNormal =() => this.notification = ""
 }
